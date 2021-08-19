6feb0cd4a5d89af1a9965631ea61576b748963bb
magbanum <nighotshantanu@gmail.com>
2021-08-19 11:28:32 +0530

    api created-2

--- oneforallblog/settings.py
+++ oneforallblog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1zhqu2zp_d&3m^zda#5oz1kxqw*1l(9u6xhfoadrfmxgk=cjtk'
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
